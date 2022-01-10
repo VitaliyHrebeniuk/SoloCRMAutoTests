d351c99790ba5fe4b90c7a0d920a53079c49b81e
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2022-01-10 11:39:09 +0200

    10.01.2022

--- src/test/java/com/ui/ManagerCreateDealFixSMM.java
+++ src/test/java/com/ui/ManagerCreateDealFixSMM.java
@@ -16,8 +16,9 @@ import java.util.concurrent.TimeUnit;
 public class ManagerCreateDealFixSMM {
     WebDriver driver;
     private String user2FaCode;
+    String managerTokenWith2FA;
     RequestSpecification request = RestAssured.given();
-    private String managerTokenWith2FA;
+    final String URL = "https://test-api.solo-crm.com/";
     private String dealID;
 
     @BeforeClass
@@ -26,11 +27,11 @@ public class ManagerCreateDealFixSMM {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         GenerateUserTokenWith2FaForManager generateUserTokenWith2FaForManager = new GenerateUserTokenWith2FaForManager();
-        //generateUserTokenWith2FaForManager.set2faForAccount();
-        this.user2FaCode = generateUserTokenWith2FaForManager.get2FaCode();
+        this.managerTokenWith2FA = generateUserTokenWith2FaForManager.set2faForAccount();
+        this.user2FaCode = generateUserTokenWith2FaForManager.manager2FaCode;
     }
     @Test
-    public void createFixSMM(){
+    public void createFixSMM() throws InterruptedException {
         // Login
         driver.get("https://test.solo-crm.com/#/login");
         WebElement login = driver.findElement(By.cssSelector("[aria-label=Login"));
@@ -39,35 +40,35 @@ public class ManagerCreateDealFixSMM {
         password.sendKeys("132465798");
         WebElement signIn = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/main/div/div/div/div/div/div/div[2]/div/div/form/div[3]/button/div"));
         signIn.click();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        WebElement autCode = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/form/div/div[2]/div[1]/div/input"));
-        autCode.sendKeys(user2FaCode);
-        WebElement send2FA = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[3]/button[2]/div"));
-        send2FA.click();
-        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-        // Закрываю окно напоминания 2FA (убрать когда починят)
-        WebElement closeNotification2FA = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[3]/button/div"));
-        closeNotification2FA.click();
-        // Переход к партнерам
-        WebElement partnersMenu = driver.findElement(By.xpath("//*[@id=\"menu-buying\"]/div"));
-        partnersMenu.click();
-        WebElement partnersList = driver.findElement(By.xpath("//*[@id=\"buying-partners_list\"]/div[2]/div"));
-        partnersList.click();
-        WebElement partnersID = driver.findElement(By.xpath("//*[@id=\"app\"]/div[82]/main/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/input"));
-        partnersID.sendKeys("5445");
-        // Создание сделки
-        WebElement plusDeal = driver.findElement(By.xpath("//*[@id=\"partner-id_5445\"]/div/i"));
-        plusDeal.click();
-        WebElement fixSMM = driver.findElement(By.xpath("//*[@id=\"fix_zid_partner-id_5445\"]/div"));
-        fixSMM.click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //Ввожу 6-ти значный код
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/form/div/div[2]/div[1]/div/input")).sendKeys(user2FaCode);
+        driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[3]/button[2]")).click();
+
+
+
+//        // Переход к партнерам
+//        WebElement partnersMenu = driver.findElement(By.xpath("//*[@id=\"menu-buying\"]/div"));
+//        partnersMenu.click();
+//        WebElement partnersList = driver.findElement(By.xpath("//*[@id=\"buying-partners_list\"]/div[2]/div"));
+//        partnersList.click();
+//        WebElement partnersID = driver.findElement(By.xpath("//*[@id=\"app\"]/div[82]/main/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/input"));
+//        partnersID.sendKeys("5445");
+//        // Создание сделки
+//        WebElement plusDeal = driver.findElement(By.xpath("//*[@id=\"partner-id_5445\"]/div/i"));
+//        plusDeal.click();
+//        WebElement fixSMM = driver.findElement(By.xpath("//*[@id=\"fix_zid_partner-id_5445\"]/div"));
+//        fixSMM.click();
     }
-//    @AfterTest
-//    public void afterGetProfile(){
-//        request
-//                .headers("token", managerTokenWith2FA)
-//                .post(URL + "/security/status/disable/" + manager2FaCode)
-//                .then()
-//                .assertThat()
-//                .statusCode(200);
-//    }
+
+    @AfterTest
+    public void afterGetProfile(){
+        driver.quit();
+        request
+                .headers("token", managerTokenWith2FA)
+                .post(URL + "security/status/disable/" + user2FaCode)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
 }
