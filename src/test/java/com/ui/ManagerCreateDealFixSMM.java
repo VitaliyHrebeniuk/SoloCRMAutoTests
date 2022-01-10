694ff4a5ca6209ff0bca50b4f1a7939fc64a8915
Spartak213 <aberzeniya@gmail.com>
2022-01-10 10:49:07 +0200

    sd

--- src/test/java/com/ui/ManagerCreateDealFixSMM.java
+++ src/test/java/com/ui/ManagerCreateDealFixSMM.java
@@ -1,13 +1,14 @@
 package com.ui;
 
-import com.ui.token2Fa.GenerateUserTokenWith2FaForAdmin;
+import com.ui.token2Fa.GenerateUserTokenWith2FaForManager;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -15,29 +16,58 @@ import java.util.concurrent.TimeUnit;
 public class ManagerCreateDealFixSMM {
     WebDriver driver;
     private String user2FaCode;
+    RequestSpecification request = RestAssured.given();
+    private String managerTokenWith2FA;
+    private String dealID;
 
     @BeforeClass
     public void setDriver(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        GenerateUserTokenWith2FaForAdmin generateUserTokenWith2FaForAdmin = new GenerateUserTokenWith2FaForAdmin();
-        this.user2FaCode = generateUserTokenWith2FaForAdmin.get2FaCode();
+        driver.manage().window().maximize();
+        GenerateUserTokenWith2FaForManager generateUserTokenWith2FaForManager = new GenerateUserTokenWith2FaForManager();
+        //generateUserTokenWith2FaForManager.set2faForAccount();
+        this.user2FaCode = generateUserTokenWith2FaForManager.get2FaCode();
     }
     @Test
     public void createFixSMM(){
         // Login
         driver.get("https://test.solo-crm.com/#/login");
         WebElement login = driver.findElement(By.cssSelector("[aria-label=Login"));
-        login.sendKeys("aleksandr_qa");
+        login.sendKeys("manager_TEST_API");
         WebElement password = driver.findElement(By.cssSelector("[aria-label=Password]"));
-        password.sendKeys("13581358");
+        password.sendKeys("132465798");
         WebElement signIn = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/main/div/div/div/div/div/div/div[2]/div/div/form/div[3]/button/div"));
         signIn.click();
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         WebElement autCode = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[2]/form/div/div[2]/div[1]/div/input"));
         autCode.sendKeys(user2FaCode);
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebElement send2FA = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[3]/button[2]/div"));
+        send2FA.click();
+        driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+        // Закрываю окно напоминания 2FA (убрать когда починят)
+        WebElement closeNotification2FA = driver.findElement(By.xpath("//*[@id=\"app\"]/div[2]/div/div/div[3]/button/div"));
+        closeNotification2FA.click();
+        // Переход к партнерам
+        WebElement partnersMenu = driver.findElement(By.xpath("//*[@id=\"menu-buying\"]/div"));
+        partnersMenu.click();
+        WebElement partnersList = driver.findElement(By.xpath("//*[@id=\"buying-partners_list\"]/div[2]/div"));
+        partnersList.click();
+        WebElement partnersID = driver.findElement(By.xpath("//*[@id=\"app\"]/div[82]/main/div/div[2]/div/div/div/div/div/div/div/div[2]/div/div[1]/div/div[1]/div/div[1]/div/div[2]/div[1]/div[1]/input"));
+        partnersID.sendKeys("5445");
+        // Создание сделки
+        WebElement plusDeal = driver.findElement(By.xpath("//*[@id=\"partner-id_5445\"]/div/i"));
+        plusDeal.click();
+        WebElement fixSMM = driver.findElement(By.xpath("//*[@id=\"fix_zid_partner-id_5445\"]/div"));
+        fixSMM.click();
     }
-
+//    @AfterTest
+//    public void afterGetProfile(){
+//        request
+//                .headers("token", managerTokenWith2FA)
+//                .post(URL + "/security/status/disable/" + manager2FaCode)
+//                .then()
+//                .assertThat()
+//                .statusCode(200);
+//    }
 }
---
+++ src/test/java/com/ui/token2Fa/GenerateUserTokenWith2FaForManager.java
@@ -0,0 +1,68 @@
+package com.ui.token2Fa;
+
+import de.taimos.totp.TOTP;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.commons.codec.binary.Base32;
+import org.apache.commons.codec.binary.Hex;
+
+public class GenerateUserTokenWith2FaForManager {
+    final String LOGIN_TRUE = "manager_TEST_API";
+    final String PASSWORD_TRUE = "132465798";
+    public String managerTokenWithout2FA;
+    public String manager2FACode;
+    public String managerTokenWith2FA;
+    public String manager2FaCode;
+    public RequestSpecification request = RestAssured.given();
+    final String URL = "https://test-api.solo-crm.com/";
+
+
+    public String setUserTokenWithout2FA(){
+        Response response = request
+                .param("login", LOGIN_TRUE)
+                .param("password", PASSWORD_TRUE)
+                .get(URL + "profile/login");
+        managerTokenWithout2FA = response.path("data.token").toString();
+        return managerTokenWithout2FA;
+    }
+
+    public String generateUser2FACode(){
+        String setUserTokenWithout2FA = setUserTokenWithout2FA();
+        Response response = request
+                .headers("token", setUserTokenWithout2FA)
+                .get( URL + "security/generate/code/qr");
+        manager2FACode = response.path("data.security_code").toString();
+        return manager2FACode;
+    }
+
+    public String getTOTPCode(String secretKey) {
+        Base32 base32 = new Base32();
+        byte[] bytes = base32.decode(secretKey);
+        String hexKey = Hex.encodeHexString(bytes);
+        return TOTP.getOTP(hexKey);
+    }
+
+    public String get2FaCode() {
+        String secretKey = generateUser2FACode();
+        String lastCode = null;
+        int i = 1;
+        while (i == 1) {
+            String code = getTOTPCode(secretKey);
+            i += 1;
+            lastCode = code;
+        }
+        //System.out.println(lastCode);
+        return lastCode;
+    }
+
+    public String set2faForAccount(){
+        this.manager2FaCode = get2FaCode();
+        Response response = request
+                .headers("token",setUserTokenWithout2FA())
+                .post(URL + "security/verify/" + manager2FaCode);
+        this.managerTokenWith2FA = response.path("data.token").toString();
+        return managerTokenWith2FA;
+    }
+
+}
