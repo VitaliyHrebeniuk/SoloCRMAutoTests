d76e601be22654183987ccb7281812ef0536fdf6
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2022-01-12 18:48:40 +0200

    12.01.2022

---
+++ src/test/java/com/api/ticket/AdminDeleteTickenException.java
@@ -0,0 +1,44 @@
+package com.api.ticket;
+
+import com.api.token2FA.GenerateUserTokenWith2FaForAdmin;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AdminDeleteTickenException {
+    public String userTokenWith2FA;
+    public String user2FaCode;
+    RequestSpecification request = RestAssured.given();
+    final String URL = "https://test-api.solo-crm.com/";
+
+    @BeforeClass
+    private void beforeUpdateProfileAdmin() {
+        GenerateUserTokenWith2FaForAdmin generateUserTokenWith2FAForAdmin = new GenerateUserTokenWith2FaForAdmin();
+        this.userTokenWith2FA = generateUserTokenWith2FAForAdmin.set2faForAccount();
+        this.user2FaCode = generateUserTokenWith2FAForAdmin.admin2FaCode;
+    }
+
+    @Test
+    private void test(){
+        request
+                .headers("token", userTokenWith2FA)
+                .param("aff_id", 1000)
+                .param("product_id",1)
+                .delete(URL+"tickets_analytic_exception")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @AfterClass
+    public void afterUpdateUserProfile(){
+        request
+                .headers("token", userTokenWith2FA)
+                .post(URL+"security/status/disable/" + user2FaCode)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+}
---
+++ src/test/java/com/api/walletTypes/FinControlGetWalletTypesList.java
@@ -0,0 +1,43 @@
+package com.api.walletTypes;
+
+import com.api.token2FA.GenerateUserTokenWith2FaForAuditor;
+import com.api.token2FA.GenerateUserTokenWith2FaForFinControl;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class FinControlGetWalletTypesList {
+    public String userTokenWith2FA;
+    public String user2FaCode;
+    RequestSpecification request = RestAssured.given();
+    final String URL = "https://test-api.solo-crm.com/";
+
+    @BeforeClass
+    private void beforeUpdateProfileAdmin() {
+        GenerateUserTokenWith2FaForFinControl generateUserTokenWith2FaForFinControl = new GenerateUserTokenWith2FaForFinControl();
+        this.userTokenWith2FA = generateUserTokenWith2FaForFinControl.set2faForAccount();
+        this.user2FaCode = generateUserTokenWith2FaForFinControl.finControl2FaCode;
+    }
+
+    @Test
+    private void test(){
+        request
+                .headers("token", userTokenWith2FA)
+                .get(URL+"wallet/types/list")
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+
+    @AfterClass
+    public void afterUpdateUserProfile(){
+        request
+                .headers("token", userTokenWith2FA)
+                .post(URL+"security/status/disable/" + user2FaCode)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+}
