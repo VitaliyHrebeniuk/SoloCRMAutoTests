cf4aa52cb57e6483f1acf7924d934991cd4f8b18
Vitalii <vitaliy.hrebeniuk@gmail.com>
2022-08-04 14:19:52 +0300

    добавил настройки к хром драйверу

--- src/main/java/com.ui/WebDriverManagerClass.java
+++ src/main/java/com.ui/WebDriverManagerClass.java
@@ -30,7 +30,6 @@ public class WebDriverManagerClass {
                 chromeOptions.setBinary("/usr/local/bin/chromedriver");
                 chromeOptions.addArguments("--headless", "--disable-gpu", "--window-size=1024,768x24","--ignore-certificate-errors");
                 chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-                chromeOptions.setExperimentalOption("useAutomationExtension", false);
 //                System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
                 chromeOptions.addArguments("--no-sandbox", "--disable-gpu" ,"--disable-extensions" ,"--headless", "--window-size=1920,1200","--ignore-certificate-errors", "--silent", "--disable-dev-shm-usage");
                 webDriver = new ChromeDriver(chromeOptions);
