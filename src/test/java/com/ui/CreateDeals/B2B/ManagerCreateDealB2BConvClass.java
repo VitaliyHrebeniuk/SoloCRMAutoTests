5ba592f1497dffbfb4c27180c96d65d907482730
vitaliihrebeniuk <vitaliy.hrebeniuk@gmail.com>
2022-08-16 12:05:47 +0300

    дебаг

--- src/test/java/com/ui/CreateDeals/B2B/ManagerCreateDealB2BConvClass.java
+++ src/test/java/com/ui/CreateDeals/B2B/ManagerCreateDealB2BConvClass.java
@@ -213,7 +213,7 @@ public class ManagerCreateDealB2BConvClass extends BaseTest {
 
     @AfterMethod
     public void ResetCode() {
-        System.out.println(manager2FaCode);
+        System.out.println(manager2FaCode + "");
         request
                 .headers("token", managerTokenWith2FA)
                 .post(apiURL + "security/status/disable/" + manager2FaCode)
