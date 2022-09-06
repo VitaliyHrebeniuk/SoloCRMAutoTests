551b0e0a6c24da335b9fd8718b08f2f8b5c9f814
Spartak213 <aberzeniya@gmail.com>
2022-09-06 12:18:21 +0300

    06.09

--- src/main/java/com/ui/WebDriverManagerClass.java
+++ src/main/java/com/ui/WebDriverManagerClass.java
@@ -26,10 +26,10 @@ public class WebDriverManagerClass {
                  **/
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
-                //chromeOptions.addArguments("--no-sandbox", "--disable-gpu" ,"--disable-extensions" ,
-                //        "--headless", "--start-maximized","--ignore-certificate-errors", "--silent",
-                //        "--disable-dev-shm-usage", "--remote-debugging-port=9222");
-
+                chromeOptions.addArguments("--no-sandbox", "--disable-gpu" ,"--disable-extensions" ,
+                        "--headless", "--ignore-certificate-errors", "--silent",
+                        "--disable-dev-shm-usage", "--remote-debugging-port=9222");
+//                "--start-maximized",
                 webDriver = new ChromeDriver(chromeOptions);
                 webDriver.manage().window().maximize();
 
