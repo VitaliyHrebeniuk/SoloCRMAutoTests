2a31ce23d092fd69243fd98755b8e3918c31619d
Vitalii <vitaliy.hrebeniuk@gmail.com>
2022-09-15 10:28:11 +0300

    15.09.2022

---
+++ src/test/java/com/api/leads/AdminPostLeadsCreate.java
@@ -0,0 +1,57 @@
+package com.api.leads;
+
+import com.api.BaseURL;
+import com.api.token2FA.GenerateUserTokenWith2FaForAdmin;
+import io.restassured.RestAssured;
+import io.restassured.specification.RequestSpecification;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class AdminPostLeadsCreate {
+    public String userTokenWith2FA;
+    public String user2FaCode;
+    RequestSpecification request = RestAssured.given();
+    BaseURL baseURL = new BaseURL();
+    final String URL = baseURL.baseURL;
+
+    private String generateRandomNumber(){
+        double a = 20 + Math.random()*40;
+        return String.valueOf(a);
+    }
+
+    @BeforeClass
+    private void beforeUpdateProfileAdmin() {
+        GenerateUserTokenWith2FaForAdmin generateUserTokenWith2FAForAdmin = new GenerateUserTokenWith2FaForAdmin();
+        this.userTokenWith2FA = generateUserTokenWith2FAForAdmin.set2faForAccount();
+        this.user2FaCode = generateUserTokenWith2FAForAdmin.admin2FaCode;
+    }
+
+    @Test
+    private void test(){
+        request
+                .headers("token", userTokenWith2FA)
+                .param("link", "https://beta.sol" + generateRandomNumber() +"-crm.com")
+                .param("country_id", 1)
+                .param("source_type_id",1)
+                .param("source_category_id",1)
+                .param("traffic", 123)
+                .param("status" , "new")
+                .param("affiliate_id", 1)
+                .param("tagList[]", 1)
+                .post(URL+"leads/create")
+                .then()
+                .assertThat()
+                .statusCode(201);
+    }
+
+    @AfterClass
+    public void afterUpdateUserProfile(){
+        request
+                .headers("token", userTokenWith2FA)
+                .post(URL+"security/status/disable/" + user2FaCode)
+                .then()
+                .assertThat()
+                .statusCode(200);
+    }
+}
