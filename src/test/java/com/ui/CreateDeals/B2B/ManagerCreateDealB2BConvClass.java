895a1fb4dd978749d0e086042661c33fef97da79
aberzeniyaqa <aberzeniyaqa@gmail.com>
2022-09-16 11:02:37 +0300

    16.08

--- src/test/java/com/ui/CreateDeals/B2B/ManagerCreateDealB2BConvClass.java
+++ src/test/java/com/ui/CreateDeals/B2B/ManagerCreateDealB2BConvClass.java
@@ -28,7 +28,7 @@ public class ManagerCreateDealB2BConvClass extends BaseTest {
         this.security_code = generateUserTokenWith2FaForManager.manager2FACode;
     }
     @Test
-    public void createDealB2BConv() throws IOException, InterruptedException {
+    public void createDealB2BConv() throws InterruptedException {
         /**
          * Login page
          * Вводим логин, вводим пароль, нажимаем на Sign In,
